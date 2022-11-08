2be422e5b6a7dfcb6e8f05b0b698d2591b759226
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-08 21:39:25 +0000

    Add wasm_pack workflow

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,15 @@ pub fn gen_passwd(count: u8) -> String {
     utils::set_panic_hook();
     xkpasswd::gen_passwd(count)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasm_bindgen_test::*;
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    fn test_gen_passwd() {
+        assert_eq!(4, gen_passwd(3).split('.').count());
+    }
+}
