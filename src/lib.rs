812cb39df2698a4412eb40e468b6cbd5beccc5d2
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-09 11:19:49 +0000

    Remove wee_alloc & update packages

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,6 @@ mod xkpasswd;
 
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
