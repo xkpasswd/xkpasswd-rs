605397abc5f0b2f4b20061d620e508f58037c27f
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-13 20:41:52 +0000

    Refactor settings mod

--- src/settings.rs
+++ src/settings.rs
@@ -27,11 +27,9 @@ impl Default for Settings {
 
 impl Settings {
     pub fn words_count(&self, words_count: u8) -> Settings {
-        Settings {
-            words_count,
-            separators: self.separators.clone(),
-            ..(*self)
-        }
+        let mut cloned = self.clone();
+        cloned.words_count = words_count;
+        cloned
     }
 
     pub fn word_lengths(&self, min_length: u8, max_length: u8) -> Settings {
@@ -41,26 +39,21 @@ impl Settings {
             (min_length, max_length)
         };
 
-        Settings {
-            word_lengths,
-            separators: self.separators.clone(),
-            ..(*self)
-        }
+        let mut cloned = self.clone();
+        cloned.word_lengths = word_lengths;
+        cloned
     }
 
     pub fn separators(&self, separators: &str) -> Settings {
-        Settings {
-            separators: separators.chars().collect(),
-            ..(*self)
-        }
+        let mut cloned = self.clone();
+        cloned.separators = separators.chars().collect();
+        cloned
     }
 
     pub fn padding_digits(&self, prefix: u8, suffix: u8) -> Settings {
-        Settings {
-            separators: self.separators.clone(),
-            padding_digits: (prefix, suffix),
-            ..(*self)
-        }
+        let mut cloned = self.clone();
+        cloned.padding_digits = (prefix, suffix);
+        cloned
     }
 
     pub fn rand_separator(&self) -> char {
@@ -75,23 +68,23 @@ impl Settings {
 
     pub fn rand_prefix(&self) -> String {
         let (prefix, _) = self.padding_digits;
-        self.rand_digits(prefix)
+        rand_digits(prefix)
     }
 
     pub fn rand_suffix(&self) -> String {
         let (_, suffix) = self.padding_digits;
-        self.rand_digits(suffix)
+        rand_digits(suffix)
+    }
+}
+
+fn rand_digits(count: u8) -> String {
+    if count == 0 {
+        return "".to_string();
     }
 
-    fn rand_digits(&self, count: u8) -> String {
-        if count == 0 {
-            return "".to_string();
-        }
-
-        let mut rng = rand::thread_rng();
-        let padding_digits: u8 = Uniform::from(10..100).sample(&mut rng);
-        padding_digits.to_string()
-    }
+    let mut rng = rand::thread_rng();
+    let padding_digits: u8 = Uniform::from(10..100).sample(&mut rng);
+    padding_digits.to_string()
 }
 
 #[cfg(test)]
@@ -125,8 +118,9 @@ mod tests {
         );
         assert_eq!((0, DEFAULT_DIGITS_COUNT), settings.padding_digits);
 
-        let settings = Settings::default().words_count(123);
-        assert_eq!(123, settings.words_count);
+        // overriding with multiple calls
+        let other_settings = settings.words_count(123);
+        assert_eq!(123, other_settings.words_count);
     }
 
     #[test]
@@ -143,11 +137,12 @@ mod tests {
         );
         assert_eq!((0, DEFAULT_DIGITS_COUNT), settings.padding_digits);
 
-        let settings = Settings::default().word_lengths(5, 5);
-        assert_eq!((5, 5), settings.word_lengths); // equal values
+        // overriding with multiple calls
+        let other_settings = settings.word_lengths(5, 5);
+        assert_eq!((5, 5), other_settings.word_lengths); // equal values
 
-        let settings = Settings::default().word_lengths(6, 4);
-        assert_eq!((4, 6), settings.word_lengths); // min/max corrected
+        let other_settings = settings.word_lengths(6, 4);
+        assert_eq!((4, 6), other_settings.word_lengths); // min/max corrected
     }
 
     #[test]
@@ -161,28 +156,9 @@ mod tests {
         assert_eq!(DEFAULT_WORD_LENGTHS, settings.word_lengths);
         assert_eq!((0, DEFAULT_DIGITS_COUNT), settings.padding_digits);
 
-        let settings = Settings::default().separators("");
-        assert_eq!(vec![] as Vec<char>, settings.separators);
-    }
-
-    #[test]
-    fn test_rand_separator() {
-        let symbols = "abc123";
-        let settings = Settings::default().separators(symbols);
-        let separator_chars = symbols.chars().collect::<Vec<char>>();
-
-        for _ in 1..10 {
-            let separator = settings.rand_separator();
-            assert_eq!(true, separator_chars.contains(&separator));
-        }
-
-        // Empty separators list
-        let settings = Settings::default().separators("");
-
-        for _ in 1..10 {
-            let separator = settings.rand_separator();
-            assert_eq!('\0', separator);
-        }
+        // overriding with multiple calls
+        let other_settings = settings.separators("");
+        assert_eq!(vec![] as Vec<char>, other_settings.separators);
     }
 
     #[test]
@@ -199,7 +175,28 @@ mod tests {
             settings.separators
         );
 
-        let settings = Settings::default().padding_digits(0, 0);
-        assert_eq!((0, 0), settings.padding_digits);
+        // overriding with multiple calls
+        let other_settings = settings.padding_digits(0, 0);
+        assert_eq!((0, 0), other_settings.padding_digits);
+    }
+
+    #[test]
+    fn test_rand_separator() {
+        let symbols = "abc123";
+        let settings = Settings::default().separators(symbols);
+        let separator_chars = symbols.chars().collect::<Vec<char>>();
+
+        for _ in 1..10 {
+            let separator = settings.rand_separator();
+            assert_eq!(true, separator_chars.contains(&separator));
+        }
+
+        // empty separators list
+        let other_settings = settings.separators("");
+
+        for _ in 1..10 {
+            let separator = other_settings.rand_separator();
+            assert_eq!('\0', separator);
+        }
     }
 }
