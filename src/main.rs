c1dc904d422ff4f3247e65b8078e3670285efd64
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-15 23:25:16 +0000

    Add word_transforms

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use settings::*;
 
 fn main() {
     let pass_generator = Xkpasswd::new();
-    let settings = &Settings::default()
+    let settings = Settings::default()
         .with_words_count(3)
-        .with_word_lengths(5, 8)
-        .with_separators("._-")
+        .with_word_lengths(4, 8)
+        .with_separators(".")
         .with_padding_digits(0, 2)
         .with_padding_symbols("!@#$%^&*-_=+:|~?/;")
-        .with_padding_symbol_lengths(0, 2);
-    println!("{}", pass_generator.gen_pass(settings));
+        .with_padding_symbol_lengths(0, 2)
+        .with_word_transforms(WordTransform::LOWERCASE | WordTransform::UPPERCASE)
+        .expect("Invalid settings");
+    println!("{}", pass_generator.gen_pass(&settings));
 }
