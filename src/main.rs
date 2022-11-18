49aab61b3721a6c6017b5b8ed28850c68a4d86e7
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-18 15:55:23 +0000

    Return Result for with_words_count()

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ use settings::*;
 
 fn main() {
     let pass_generator = Xkpasswd::new();
-    let settings = Settings::default()
-        .with_words_count(3)
+    let settings = build_settings().expect("Invalid settings");
+    println!("{}", pass_generator.gen_pass(&settings));
+}
+
+fn build_settings() -> Result<Settings, &'static str> {
+    Settings::default()
+        .with_words_count(3)?
         .with_word_lengths(4, 8)
         .with_separators(".")
         .with_padding_digits(0, 2)
         .with_padding_symbols("!@#$%^&*-_=+:|~?/;")
         .with_padding_symbol_lengths(0, 2)
         .with_word_transforms(WordTransform::LOWERCASE | WordTransform::UPPERCASE)
-        .expect("Invalid settings");
-    println!("{}", pass_generator.gen_pass(&settings));
 }
