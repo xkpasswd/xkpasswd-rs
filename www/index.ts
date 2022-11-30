331628de7ad9c993e54d8227f992e64a15fb09a2
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-11-30 23:16:57 +0000

    Add UI

--- www/index.ts
+++ www/index.ts
@@ -1,51 +1,29 @@
 import init, * as xkpasswd from 'xkpasswd';
 
-async function run() {
-  await init();
+const PRESET_MAPPER: Record<string, xkpasswd.Preset> = {
+  default: xkpasswd.Preset.Default,
+  appleId: xkpasswd.Preset.AppleID,
+  ntlm: xkpasswd.Preset.WindowsNtlmV1,
+  secq: xkpasswd.Preset.SecurityQuestions,
+  web16: xkpasswd.Preset.Web16,
+  web32: xkpasswd.Preset.Web32,
+  wifi: xkpasswd.Preset.Wifi,
+  xkcd: xkpasswd.Preset.Xkcd,
+};
 
+async function bootstrap() {
+  await init();
   const pass = new xkpasswd.Xkpasswd();
 
-  try {
-    const customSettings = new xkpasswd.Settings()
-      .withWordsCount(3)
-      .withWordLengths(undefined, 8)
-      .withSeparators('.')
-      .withPaddingDigits(undefined, 2)
-      .withPaddingSymbols('!@#$%^&*-_=+:|~?/;')
-      .withPaddingSymbolLengths(undefined, 2)
-      .withWordTransforms(
-        xkpasswd.WordTransform.Lowercase | xkpasswd.WordTransform.Uppercase
-      );
-
-    appendPasswd('Custom', pass.genPass(customSettings));
-  } catch (exc) {
-    console.warn(exc);
-  }
-
-  const presetTitles = [
-    'AppleID',
-    'Default',
-    'WindowsNTLMv1',
-    'SecurityQuestions',
-    'Web16',
-    'Web32',
-    'Wifi',
-    'XKCD',
-  ];
-
-  [
-    xkpasswd.Preset.AppleID,
-    xkpasswd.Preset.Default,
-    xkpasswd.Preset.WindowsNtlmV1,
-    xkpasswd.Preset.SecurityQuestions,
-    xkpasswd.Preset.Web16,
-    xkpasswd.Preset.Web32,
-    xkpasswd.Preset.Wifi,
-    xkpasswd.Preset.Xkcd,
-  ].forEach((preset, idx) => {
-    const settings = xkpasswd.Settings.fromPreset(preset);
-    appendPasswd(presetTitles[idx], pass.genPass(settings));
-  });
+  document
+    .querySelector('#presets-select')
+    ?.addEventListener('change', (event) => {
+      const target = event.target as HTMLSelectElement;
+      console.log('target', target.innerText);
+      const preset = PRESET_MAPPER[target.value];
+      const settings = xkpasswd.Settings.fromPreset(preset);
+      appendPasswd('Preset', pass.genPass(settings));
+    });
 }
 
 function appendPasswd(title: string, passwd: string) {
@@ -53,18 +31,15 @@ function appendPasswd(title: string, passwd: string) {
 
   const span = document.createElement('span');
   span.textContent = `${title}:`;
-  span.setAttribute('style', 'color: darkgray; font-weight: bold;');
+  span.className = 'font-bold text-slate-400 mr-1';
   div.appendChild(span);
 
   const pre = document.createElement('pre');
   pre.textContent = passwd;
-  pre.setAttribute(
-    'style',
-    'display: inline; font-weight: bold; margin-left: 0.5em;'
-  );
+  pre.className = 'inline-block font-bold font-mono hover:underline';
   div.appendChild(pre);
 
   document.body.append(div);
 }
 
-run();
+bootstrap();
