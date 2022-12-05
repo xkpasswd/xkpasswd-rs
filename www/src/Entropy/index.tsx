1905ca3455cbd08a600897504670cc34f85bdd9e
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-12-05 15:12:25 +0000

    Add Entropy recommendation

--- www/src/Entropy/index.tsx
+++ www/src/Entropy/index.tsx
@@ -1,9 +1,17 @@
 import * as xkpasswd from '../../xkpasswd/xkpasswd';
 import './styles.css';
+import notBadImage from '/not-bad.png';
+import rageFaceImage from '/rage-face.png';
+
+const MIN_RECOMMENDED_ENTROPY_BLIND = 78;
+const MIN_RECOMMENDED_ENTROPY_SEEN = 52;
 
 type Props = {
   entropy?: xkpasswd.Entropy;
 };
+
+const entropyClassName = (good: boolean) =>
+  good ? 'entropy entropy-good' : 'entropy entropy-bad';
 
 const Entropy = ({ entropy }: Props) => {
   if (!entropy) {
@@ -11,12 +19,23 @@ const Entropy = ({ entropy }: Props) => {
   }
 
   const entropyBlindMin = (
-    <span className="entropy" key="entropy-blind-min">
+    <span
+      className={entropyClassName(
+        entropy.blind_min >= MIN_RECOMMENDED_ENTROPY_BLIND
+      )}
+      key="entropy-blind-min"
+    >
       {entropy.blind_min}
     </span>
   );
+
   const entropyBlindMax = (
-    <span className="entropy" key="entropy-blind-max">
+    <span
+      className={entropyClassName(
+        entropy.blind_max >= MIN_RECOMMENDED_ENTROPY_BLIND
+      )}
+      key="entropy-blind-max"
+    >
       {entropy.blind_max}
     </span>
   );
@@ -33,15 +52,38 @@ const Entropy = ({ entropy }: Props) => {
         ];
 
   const entropySeen = [
-    <span className="entropy" key="entropy-seen">
+    <span
+      className={entropyClassName(entropy.seen >= MIN_RECOMMENDED_ENTROPY_SEEN)}
+      key="entropy-seen"
+    >
       {entropy.seen}
     </span>,
-    ' bits with full knowledge!',
+    ' bits with full knowledge, ',
   ];
+
+  const recommendation =
+    entropy.blind_min >= MIN_RECOMMENDED_ENTROPY_BLIND &&
+    entropy.seen >= MIN_RECOMMENDED_ENTROPY_SEEN
+      ? [
+          'which is not bad!',
+          <img
+            className="entropy-img not-bad"
+            key="entropy-not-bad"
+            src={notBadImage}
+          />,
+        ]
+      : [
+          'which is not good!',
+          <img
+            className="entropy-img rage-face"
+            key="entropy-rage-face"
+            src={rageFaceImage}
+          />,
+        ];
 
   return (
     <div className="mt-4">
-      <span>{[...entropyBlind, ...entropySeen]}</span>
+      <span>{[...entropyBlind, ...entropySeen, ...recommendation]}</span>
     </div>
   );
 };
