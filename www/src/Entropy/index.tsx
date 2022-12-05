1df0b532a68a1d8a0ee0b23fb4054700f66ab161
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-12-05 18:20:44 +0000

    Add feels-good.png & resize logo

--- www/src/Entropy/index.tsx
+++ www/src/Entropy/index.tsx
@@ -1,92 +1,108 @@
 import * as xkpasswd from '../../xkpasswd/xkpasswd';
 import './styles.css';
+import feelsGoodImage from '/feels-good.png';
 import notBadImage from '/not-bad.png';
 import rageFaceImage from '/rage-face.png';
 
-const MIN_RECOMMENDED_ENTROPY_BLIND = 78;
-const MIN_RECOMMENDED_ENTROPY_SEEN = 52;
+const NOT_BAD_ENTROPY_BLIND = 78;
+const NOT_BAD_ENTROPY_SEEN = 52;
+const GREAT_ENTROPY_BLIND = NOT_BAD_ENTROPY_BLIND * 1.5;
+const GREAT_ENTROPY_SEEN = NOT_BAD_ENTROPY_SEEN * 1.5;
 
 type Props = {
   entropy?: xkpasswd.Entropy;
 };
 
-const entropyClassNames = (good: boolean) =>
-  good ? 'entropy entropy-good' : 'entropy entropy-bad';
+type BitsProps = {
+  value: number;
+  seen?: boolean;
+};
+
+const Bits = ({ value, seen = false }: BitsProps) => {
+  const safeEntropy = seen ? NOT_BAD_ENTROPY_SEEN : NOT_BAD_ENTROPY_BLIND;
+  const classNames =
+    value >= safeEntropy ? 'entropy entropy-good' : 'entropy entropy-bad';
+  return <span className={classNames}>{value}</span>;
+};
 
 const Entropy = ({ entropy }: Props) => {
   if (!entropy) {
     return null;
   }
 
-  const entropyBlindMin = (
-    <span
-      className={entropyClassNames(
-        entropy.blind_min >= MIN_RECOMMENDED_ENTROPY_BLIND
-      )}
-      key="entropy-blind-min"
-    >
-      {entropy.blind_min}
-    </span>
-  );
-
-  const entropyBlindMax = (
-    <span
-      className={entropyClassNames(
-        entropy.blind_max >= MIN_RECOMMENDED_ENTROPY_BLIND
-      )}
-      key="entropy-blind-max"
-    >
-      {entropy.blind_max}
-    </span>
-  );
+  const entropyBlindMin = <Bits value={entropy.blind_min} />;
+  const entropyBlindMax = <Bits value={entropy.blind_max} />;
 
   const entropyBlind =
-    entropy.blind_min == entropy.blind_max
-      ? [`Btw, it's entropy is of `, entropyBlindMin, ' bits blind & ']
-      : [
-          `Btw, it's entropy is between `,
-          entropyBlindMin,
-          ' and ',
-          entropyBlindMax,
-          ' bits blind & ',
-        ];
-
-  const entropySeen = [
-    <span
-      className={entropyClassNames(
-        entropy.seen >= MIN_RECOMMENDED_ENTROPY_SEEN
-      )}
-      key="entropy-seen"
-    >
-      {entropy.seen}
-    </span>,
-    ' bits with full knowledge, ',
-  ];
-
-  const recommendation =
-    entropy.blind_min >= MIN_RECOMMENDED_ENTROPY_BLIND &&
-    entropy.seen >= MIN_RECOMMENDED_ENTROPY_SEEN
-      ? [
-          'which is not bad!',
-          <img
-            className="entropy-img not-bad"
-            key="entropy-not-bad"
-            src={notBadImage}
-          />,
-        ]
-      : [
-          'which is not good!',
-          <img
-            className="entropy-img rage-face"
-            key="entropy-rage-face"
-            src={rageFaceImage}
-          />,
-        ];
+    entropy.blind_min == entropy.blind_max ? (
+      <>
+        {`Btw, it's entropy is of `}
+        {entropyBlindMin}
+        {' bits blind & '}
+      </>
+    ) : (
+      <>
+        {`Btw, it's entropy is between `}
+        {entropyBlindMin}
+        {' and '}
+        {entropyBlindMax}
+        {' bits blind & '}
+      </>
+    );
 
   return (
     <div className="section">
-      <span>{[...entropyBlind, ...entropySeen, ...recommendation]}</span>
+      <span>
+        {entropyBlind}
+        <Bits seen value={entropy.seen} />
+        {' bits with full knowledge, '}
+        <Ratings entropy={entropy} />
+      </span>
+      <span className="entropy-recommendation">
+        {`(It's recommended to keep `}
+        <Bits value={NOT_BAD_ENTROPY_BLIND} />
+        {' bits blind & '}
+        <Bits seen value={NOT_BAD_ENTROPY_SEEN} />
+        {' bits with full knowledge.)'}
+      </span>
     </div>
+  );
+};
+
+const Ratings = ({ entropy }: Props) => {
+  if (!entropy) {
+    return null;
+  }
+
+  if (
+    entropy.blind_min < NOT_BAD_ENTROPY_BLIND ||
+    entropy.seen < NOT_BAD_ENTROPY_SEEN
+  ) {
+    return (
+      <>
+        {'which is not good!'}
+        <img className="entropy-img rage-face" src={rageFaceImage} />
+      </>
+    );
+  }
+
+  if (
+    entropy.blind_min >= GREAT_ENTROPY_BLIND &&
+    entropy.seen >= GREAT_ENTROPY_SEEN
+  ) {
+    return (
+      <>
+        {'which is great!'}
+        <img className="entropy-img feels-good" src={feelsGoodImage} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      {'which is not bad!'}
+      <img className="entropy-img not-bad" src={notBadImage} />
+    </>
   );
 };
 
